21acce1cd742b994415f17c3f481f7fba18f639e
relberger <relberger96@gmail.com>
2018-11-29 13:59:10 -0500

    fixed some bugs

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -2,7 +2,6 @@ import entities.Direction;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -81,7 +80,7 @@ public class SpaceInvaders extends JFrame {
                 gameBoard.movement = Direction.RIGHT;
                 gameBoard.moveShooter();
             } else if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
-                gameBoard.shoot(gameBoard.getShooter());
+                gameBoard.shooterShoot(gameBoard.getShooter());
                 repaint();
                 setTitle(title + gameBoard.getScore() + lives + gameBoard.getShooter().getLives() );
             }
